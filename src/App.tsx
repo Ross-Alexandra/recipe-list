d5550bf414d06e8928b8bbf63956a9b7b98a103f
Ross Alexandra <Ross-Alexandra@outlook.com>
2022-08-25 20:36:09 -0700

    setup eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import {setupIonicReact} from '@ionic/react';
+import { setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 setupIonicReact();
 
