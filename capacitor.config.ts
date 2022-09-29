09fd06727acfd769f85cee173204c022124eb1ab
Ross Alexandra <Ross-Alexandra@outlook.com>
2022-09-28 17:05:25 -0700

    Updated app name, removed icon.svg

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'rossalexandra.recipeList',
-  appName: 'recipe-list',
-  webDir: 'build',
-  bundledWebRuntime: false
+    appId: 'rossalexandra.recipeList',
+    appName: 'Recilist',
+    webDir: 'build',
+    bundledWebRuntime: false
 };
 
 export default config;
